e3bcc622baf4f9c20d91a5ff551272553a07d858
Kellly <kcdonlan@yahoo.com>
2022-02-19 13:04:49 -0500

    fixed connection issues

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,28 +1,34 @@
 const router = require('express').Router();
 const fs = require('fs');
 
-const savedNote = require('../../Develop/db/db.json');
+let noteData  = require('../../Develop/db/db.json');
 
 // Unique ID creator
 const { v4: uuidv4 } = require('uuid');
 
 // GET request
-router.get('/api/notes', (req, res) => {
-    res.json(savedNote);
+router.get('/notes', (req, res) => {
+    let results = noteData;
+
+    if(results) {
+        res.json(results);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 // POST Request
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const data = req.body;
     // creating a unique id for the note
     data.id = uuidv4(data.id);
 
-    savedNote.push(data);
+    noteData.push(data);
     
-    fs.writeFile('/db/db.json', JSON.stringify(savedNote), (err) => {
+    fs.writeFile('/db/db.json', JSON.stringify(noteData), (err) => {
         if(err) {
             res.status(400).send("Oops, something didn't go right.");
-            return;
         }
         else {
             console.log("YEAH! Note has been saved.")
@@ -31,15 +37,15 @@ router.post('/api/notes', (req, res) => {
         res.json(true);
 });
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
 
-    const savedNoteId = req.params.id;
+    const noteDataId = req.params.id;
 
-    savedNote = savedNote.filter((notes, index) => {
+    noteData = noteData.filter((notes, index) => {
         console.log(index)
-        return savedNoteId !== notes.id;
+        return noteDataId !== notes.id;
     });
-    fs.writeFile('/db/db.json', JSON.stringify(savedNote), (err) => {
+    fs.writeFile('/db/db.json', JSON.stringify(noteData), (err) => {
         if(err)
             res.status(400).send("Oops, something didn't go right.");
         
